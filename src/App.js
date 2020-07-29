892b0dd8b91159a2f3e68f925b9b1012c35c012e
Lucas0liveira <lucasoliveira96@outlook.com>
2020-07-29 09:58:00 -0400

    dados iniciais atualizados

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
+import BannerMain from './components/BannerMain'
+import Button from './components/Button'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
 import Menu from './components/Menu'
+import dadosIniciais from './dados_iniciais.json'
 
 function App() {
   return (
     <div>
-      <Menu/>
+
+      <Menu />
+      
+      <BannerMain 
+        videoTitle = {dadosIniciais.categorias[1].videos[1].titulo}
+        url={dadosIniciais.categorias[1].videos[1].url}
+        videoDescription = {"O que é Front-end? Trabalhando na área."} 
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />
+      
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />
+
+      <Footer/>
+
     </div>
   );
 }
